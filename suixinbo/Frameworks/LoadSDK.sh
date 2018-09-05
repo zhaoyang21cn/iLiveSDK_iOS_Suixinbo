ef0d7dd263c86fe5d968785b1d3e919d41654cd7
iliveSDK <githubzt@gmail.com>
2018-09-05 11:10:19 +0800

    Update LoadSDK.sh

--- suixinbo/Frameworks/LoadSDK.sh
+++ suixinbo/Frameworks/LoadSDK.sh
@@ -4,7 +4,7 @@
 # 加载各个模块SDK
 # 版本号如下
 echo "|-------------------------------sdk version--------------------------------|"
-ILiveSDKVersion="ILiveSDK_1.9.4.14015"
+ILiveSDKVersion="ILiveSDK_1.9.4.14018"
 IMSDKSDKVersion="IMSDK_2.5.6.11389.11327"
 AVSDKVersion="AVSDK_1.9.9.1014.39687"
 BeautySDKVersion="BeautySDK"
